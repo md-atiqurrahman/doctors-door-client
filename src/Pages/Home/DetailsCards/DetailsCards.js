adbcd8cb030aab9e96852f84dbbcfdda7f5671b8
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2023-07-12 20:51:28 +0600

    make changes in code of detailsCards component

--- src/Pages/Home/DetailsCards/DetailsCards.js
+++ src/Pages/Home/DetailsCards/DetailsCards.js
@@ -2,47 +2,47 @@ import React from "react";
 import clock from "../../../assets/icons/clock.svg";
 import marker from "../../../assets/icons/marker.svg";
 import phone from "../../../assets/icons/phone.svg";
-import Card from "./Card";
+import DetailsCard from "./DetailsCard";
 
-const DetailsCard = () => {
-  const detailsCard = [
+const DetailsCards = () => {
+  const cardData = [
     {
       id: 1,
-      image: clock,
+      icon: clock,
       title: "Opening Hours",
-      subTitle: "From 10:30 am to 9 pm",
+      description: "From 10:30 am to 9 pm",
       bgColor: "bg-gradient-to-r from-secondary to-primary",
     },
     {
       id: 2,
-      image: marker,
+      icon: marker,
       title: "Visit our location",
-      subTitle: "Brooklyn, NY 10036, United States",
+      description: "Brooklyn, NY 10036, United States",
       bgColor: "bg-accent",
     },
     {
       id: 3,
-      image: phone,
+      icon: phone,
       title: "Contact us now",
-      subTitle: "+000 123 456789",
+      description: "+000 123 456789",
       bgColor: "bg-gradient-to-r from-secondary to-primary",
     },
   ];
   return (
     <div className="mt-[64px]">
       <div className="grid grid-cols-3 gap-[24px]">
-        {detailsCard.map((card) => (
-          <Card
+        {cardData.map((card) => (
+          <DetailsCard
             key={card.id}
-            image={card.image}
+            icon={card.icon}
             title={card.title}
-            subTitle={card.subTitle}
+            description={card.description}
             bgColor={card.bgColor}
-          ></Card>
+          ></DetailsCard>
         ))}
       </div>
     </div>
   );
 };
 
-export default DetailsCard;
+export default DetailsCards;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
-import DetailsCard from "../DetailsCard/DetailsCard";
+import DetailsCards from "../DetailsCards/DetailsCards";
 
 const Home = () => {
   return (
     <div className="mx-[21px]">
       <Banner></Banner>
-      <DetailsCard></DetailsCard>
+      <DetailsCards></DetailsCards>
     </div>
   );
 };
