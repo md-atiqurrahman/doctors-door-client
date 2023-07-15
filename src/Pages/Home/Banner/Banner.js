c47e10e6f9f7b1708ad93a0b3e553bc0e96fb3b2
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2023-07-15 13:29:52 +0600

    make the speciality section responsive for small devices

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,19 +1,30 @@
 import React from "react";
 import chair from "../../../assets/images/chair.png";
+import './Banner.css';
 
 const Banner = () => {
   return (
-    <div className="hero bg-transparent lg:bg-bannerBg  min-h-0 lg:min-h-screen mt-[10px]">
-      <div className="hero-content gap-[24px] px-0 lg:px-[33px] py-0 flex-col lg:flex-row-reverse">
-        <img className="w-full lg:w-1/2 rounded-lg shadow-2xl " src={chair} alt="" />
-        <div>
-          <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
-          <p className="pt-[14px] pb-[45px]">
-            Dentists help keep your teeth healthy with regular dental
-            check-ups and cleanings. They can also perform a variety of oral
-            health treatments.
-          </p>
-          <button className="btn bg-gradient-to-r from-secondary to-primary text-white">Get Started</button>
+    <div>
+      <div
+        className="hero bg-no-repeat bg-transparent lg:bg-bannerBg  mt-[10px]"
+      >
+        <div className="hero-content  gap-[24px] px-0 lg:px-[33px] py-0 flex-col lg:flex-row-reverse">
+          <img
+            className="w-full lg:w-1/2 rounded-lg shadow-2xl "
+            src={chair}
+            alt=""
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+            <p className="pt-[14px] pb-[45px]">
+              Dentists help keep your teeth healthy with regular dental
+              check-ups and cleanings. They can also perform a variety of oral
+              health treatments.
+            </p>
+            <button className="btn bg-gradient-to-r from-secondary to-primary text-white">
+              Get Started
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/Pages/Home/Speciality/Speciality.js
+++ src/Pages/Home/Speciality/Speciality.js
@@ -3,15 +3,15 @@ import treatment from "../../../assets/images/treatment.png";
 
 const Speciality = () => {
   return (
-    <div className="mt-[154px] flex justify-center items-center  gap-[102px] pl-[196px] pr-[140px]">
+    <div className="mt-[56px] lg:mt-[154px] flex flex-col lg:flex-row justify-center items-center  gap-[37px] lg:gap-[102px] pl-0 lg:pl-[196px] pr-0 lg:pr-[140px]">
       <div>
-        <img className="max-w-sm lg:max-w-sm rounded-[8px]" src={treatment} alt="" />
+        <img className="max-w-[322px] lg:max-w-sm rounded-[8px]" src={treatment} alt="" />
       </div>
       <div>
         <h2 className="text-accent text-[48px] font-bold">
           Exceptional Dental Care, on Your Terms
         </h2>
-        <p className="text-black text-[16px] text-normal">
+        <p className="text-black text-[16px] text-normal mt-2 text-justify">
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsumis that it has a more-or-less normal distribution of
